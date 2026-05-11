370778294f551a882b31af1c49bd8b1d48142c6e
Kenny <36441237+kennypassenier@users.noreply.github.com>
2026-05-11 19:49:53 +0200

    Update versions: 1778521793

--- scripts/youtube/loader.user.js
+++ scripts/youtube/loader.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         youtube Loader
 // @namespace    Custom.Automation
-// @version      1778521254
+// @version      1778521793
 // @description
 // @author       Kenny
 // @match        *.youtube.com
 
 // @run-at       document-idle
 // @grant        none
-// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/youtube/logic.js?token=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
+// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/youtube/logic.js
 // ==/UserScript==
