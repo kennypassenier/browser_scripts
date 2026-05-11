65b11b78f1718eac93f7baa07e861c950bc9d2af
Kenny <36441237+kennypassenier@users.noreply.github.com>
2026-05-12 01:24:29 +0200

    Move redditold/logic.js to reddit/logic-oldreddit.js; remove redditold dir

--- scripts/reddit/loader.user.js
+++ scripts/reddit/loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         reddit
 // @namespace    Custom.Automation
-// @version      1778541740
+// @version      1778541869
 // @description
 // @author       Kenny
 // @match        *://*.reddit.com/*
@@ -9,8 +9,8 @@
 // @downloadURL  https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/reddit/loader.user.js
 // @run-at       document-idle
 // @grant        none
-// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/reddit/logic-redirect.js?v=1778541740
-// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/redditold/logic.js?v=1778541740
-// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/reddit/logic-comments.js?v=1778541740
-// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/reddit/logic.js?v=1778541740
+// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/reddit/logic-redirect.js?v=1778541869
+// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/reddit/logic-oldreddit.js?v=1778541869
+// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/reddit/logic-comments.js?v=1778541869
+// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/reddit/logic.js?v=1778541869
 // ==/UserScript==
