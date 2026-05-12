25eaf05ac91a788829ea1cdc912b52261e56acc2
Kenny <36441237+kennypassenier@users.noreply.github.com>
2026-05-12 03:22:55 +0200

    Update versions: 1778548975

--- scripts/crosssuite/loader.user.js
+++ scripts/crosssuite/loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         crosssuite
 // @namespace    Custom.Automation
-// @version      1778548747
+// @version      1778548975
 // @description  
 // @author       Kenny
 // @match        *://agenda.crossuite.com
@@ -10,5 +10,5 @@
 
 // @run-at       document-idle
 // @grant        none
-// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/crosssuite/logic.js?v=1778548747
+// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/crosssuite/logic.js?v=1778548975
 // ==/UserScript==
--- scripts/play/loader.user.js
+++ scripts/play/loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         play
 // @namespace    Custom.Automation
-// @version      1778548747
+// @version      1778548975
 // @description  
 // @author       Kenny
 // @match        *://*.play.tv/*
@@ -10,5 +10,5 @@
 
 // @run-at       document-idle
 // @grant        none
-// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/play/logic.js?v=1778548747
+// @require      https://raw.githubusercontent.com/kennypassenier/browser_scripts/refs/heads/main/scripts/play/logic.js?v=1778548975
 // ==/UserScript==
